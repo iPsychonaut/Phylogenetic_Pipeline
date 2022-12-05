7e5790e255ee548891cc35d6bf1e21a656911fd0
Ian M. B <99409346+iPsychonaut@users.noreply.github.com>
2022-12-05 15:48:43 -0800

    Updated to Match GUI 12/5

--- BioPyPhyloMain.py
+++ BioPyPhyloMain.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Nov 30 11:19:41 2022
 
-Main Phylogenetic GUI
+Main Phylogenetic Program
 
 @author: ian.michael.bollinger@gmail.com
 """
@@ -10,8 +10,8 @@ Main Phylogenetic GUI
 # IMPORT LIBRARIES
 ###############################################################################
 from BioPyNCBISeqs import search_ncbi
-from BioPyTreeGen import run_iqtree, tree_from_alignment, gen_boostrap_consensus_tree
-from BioPySeqAlignments import MUSCLE_alignment, trimAI_alignment, fasta_to_strict_phylip, fasta_to_relaxed_phylip
+from BioPyTreeGen import run_iqtree, gen_nj_tree, gen_boostrap_consensus_tree
+from BioPySeqAlign import MUSCLE_alignment, trimAI_alignment
 from BioPyFolderSeqs import combine_fastas
 import os
 from datetime import datetime
@@ -19,38 +19,38 @@ from datetime import datetime
 ###############################################################################
 # DEBUG WORKSPACE
 ###############################################################################
-# Set max number of records to return
-return_number = 100
-# Set user Email for Entrez
-user_email = 'ian.michael.bollinger@gmail.com'
+# Pick one of the following:
+    
+# 1) Combined Fasta from Folder
+# Set fasta directory
+save_path = 'TEST_IMPORT'
+# Generate Combined Sequence List
+combined_path = combine_fastas(save_path)
 
-# # Set fasta directory
-# save_path = 'TEST IMPORT'
-# # Generate Combined Sequence List
-# combined_path = combine_fastas(save_path)
+# # 2) Combined Fasta from NCBI Search
+# # Set search term
+# user_email = 'ian.michael.bollinger@gmail.com'
+# #PsiM, PsiD, PsiK, PsiT2, PsiT1, 
+# gene_name = 'Internal transcribed spacer'.replace(' ','_')
+# organism = 'Fungi'.replace(' ','_')
+# return_number = 25
+# search_minbp = 100
+# search_maxbp = 5000
+# search_term = f'"{gene_name}"[All Fields] AND "{organism}"[Organism] AND ("{search_minbp}"[SLEN] : "{search_maxbp}"[SLEN]) NOT "whole genome shotgun"[All Fields]'
 
-# Set search term
-#PsiM, PsiD, PsiK, PsiT2, PsiT1, 
-gene_name = 'Internal transcribed spacer'.replace(' ','_')
-organism = 'Psilocybe'.replace(' ','_')
-search_term = f'"{gene_name}"[All Fields] AND "{organism}"[Organism]'
-search_db = 'nucleotide'
-# datetime object containing current date and time
-now = datetime.now()
-# dd/mm/YY H:M:S
-dt_string = now.strftime("%Y%m%d_%H%M")
-save_path = f'{gene_name}_{organism}_{return_number}_{dt_string}'
+# search_db = 'nucleotide'
+# now = datetime.now() # datetime object containing current date and time
+# dt_string = now.strftime("%Y%m%d_%H%M")
+# save_path = f'{gene_name}_{organism}_{return_number}_{dt_string}'
 
-# Check whether the specified path exists or not
-isExist = os.path.exists(save_path)
-if not isExist:
-    # Create a new directory because it does not exist
-    os.makedirs(save_path)
+# # Check whether the specified path exists or not
+# isExist = os.path.exists(save_path)
+# if not isExist:
+#     # Create a new directory because it does not exist
+#     os.makedirs(save_path)
 
-# Download Sequences and Generate Combined Sequence List
-combined_path = search_ncbi(user_email, search_term, return_number, search_db, save_path)
-
-
+# # Download Sequences and Generate Combined Sequence List
+# combined_path = search_ncbi(user_email, search_term, return_number, search_db, save_path)
 
 ###############################################################################
 # MAIN FUNCTIONS CALL
@@ -60,13 +60,8 @@ MUSCLE_aln_path = MUSCLE_alignment(combined_path, save_path)
 
 # Trim the MUSCLE Alignment and convert to Phylip (relaxed or strict)
 trimmed_aln_path = trimAI_alignment(MUSCLE_aln_path)
-relaxed_phylip_aln_path = fasta_to_relaxed_phylip(trimmed_aln_path)
-#strict_phylip_aln_path = fasta_to_strict_phylip(trimmed_aln_path)
-
-# CONSIDER OTHER ALIGNMENTS(?): MAFFT, CLUSTALW
 
 # Creating Tree Files and Figures based on MUSCLE Alignment
-run_iqtree(relaxed_phylip_aln_path, save_path)
-tree_from_alignment(relaxed_phylip_aln_path, 'upgma')
-tree_from_alignment(relaxed_phylip_aln_path, 'nj')
-gen_boostrap_consensus_tree(relaxed_phylip_aln_path, 500)
+gen_nj_tree(trimmed_aln_path)
+gen_boostrap_consensus_tree(trimmed_aln_path, 200)
+run_iqtree(trimmed_aln_path, save_path)
