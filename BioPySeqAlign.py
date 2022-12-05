5cf749ae5a92aed372b92cff45f11a3b4d89b5a8
Ian M. B <99409346+iPsychonaut@users.noreply.github.com>
2022-12-05 15:46:28 -0800

    Updated to Match GUI 12/5

--- BioPySeqAlign.py
+++ BioPySeqAlign.py
@@ -22,6 +22,7 @@ trimAI_exe = r"C:/Users/theda/.spyder-py3/BioPy/trimal.exe"
 ###############################################################################
 # Functiont to genreate a MUSCLE Alignment
 def MUSCLE_alignment(input_alignment, save_path):   
+    
     # Build out MUSCLE Command Line
     MUSCLE_alignment_path = f"{save_path}/combined_MUSCLE.fasta"
     muscle_command = f'{muscle_exe} -align "{input_alignment}" -output "{MUSCLE_alignment_path}"'
@@ -34,10 +35,11 @@ def MUSCLE_alignment(input_alignment, save_path):
     return(MUSCLE_alignment_path)
 
 ###############################################################################
-# CLEAN-UP FUNCTIONS
+# ALIGNMENT CLEAN-UP FUNCTIONS
 ###############################################################################
 # Function to trim an alignment 
 def trimAI_alignment(input_alignment):   
+    
     # Build out trimAI Command Line
     output_trimAI_path = f"{input_alignment.replace('.fasta', '_trimAI.fasta')}"
     trimAI_command = f'{trimAI_exe} -in "{input_alignment}" -out "{output_trimAI_path}" -automated1'
@@ -47,8 +49,8 @@ def trimAI_alignment(input_alignment):
 
 # Function to Convert a combined fasta alignment into a relaxed phylip format
 def fasta_to_relaxed_phylip(input_alignment):
-    output_phylip_path = f"{input_alignment.replace('.fasta', '_relaxed')}"
-    AlignIO.convert(input_alignment, 'fasta', f'{output_phylip_path}.phylip', 'phylip-relaxed')
+    output_phylip_path = f"{input_alignment.replace('.fasta', '_relaxed.phylip')}"
+    AlignIO.convert(input_alignment, 'fasta', output_phylip_path, 'phylip-relaxed')
     return(output_phylip_path)
 
 # Function to Convert a combined fasta alignment into a strict phylip format
@@ -61,21 +63,21 @@ def fasta_to_strict_phylip(input_alignment):
             print(f'STRICT PHYLIP ID: {seq_record.id}')
             print(f'STRICT PHYLIP DESCRIPTION: {seq_record.description}')
             import_list.append(seq_record)
-    output_phylip_path = f"{input_alignment.replace('.fasta', '_strict')}"
-    SeqIO.write(import_list, f'{output_phylip_path}.phylip', "phylip")
+    output_phylip_path = f"{input_alignment.replace('.fasta', '_strict.phylip')}"
+    SeqIO.write(import_list, output_phylip_path, "phylip")
     return(output_phylip_path)
 
 ###############################################################################
 # DEBUG WORKSPACE
 ###############################################################################
 # file_name = 'combined.fasta'
-# save_path = 'C:/Users/theda/.spyder-py3/BioPy/TEST IMPORT'
-# # save_path= 'C:/Users/theda/.spyder-py3/BioPy'
-# # file_name = 'neoxalapensis.fasta'
+# save_path = 'C:/Users/theda/.spyder-py3/BioPy/TEST_IMPORT'
+# # # save_path= 'C:/Users/theda/.spyder-py3/BioPy'
+# # # file_name = 'neoxalapensis.fasta'
 # input_alignment = f'{save_path}/{file_name}'
 
 # MUSCLE_aln_path = MUSCLE_alignment(input_alignment, save_path)
 # trimmed_aln_path = trimAI_alignment(MUSCLE_aln_path)
-# strict_phylip_aln_path = fasta_to_strict_phylip(trimmed_aln_path)
-# #relaxed_phylip_aln_path = fasta_to_relaxed_phylip(trimmed_aln_path)
-# #print(relaxed_phylip_aln_path)
+# # strict_phylip_aln_path = fasta_to_strict_phylip(trimmed_aln_path)
+# relaxed_phylip_aln_path = fasta_to_relaxed_phylip(trimmed_aln_path)
+# # #print(relaxed_phylip_aln_path)
